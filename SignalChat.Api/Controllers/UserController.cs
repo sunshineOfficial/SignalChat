27e43e6f6c740fe5643c511fc707c29b9f244053
Dmitry Khrunin <thesunshineofficial@gmail.com>
2024-02-19 09:26:00 +0300

    Add GetUser route

--- SignalChat.Api/Controllers/UserController.cs
+++ SignalChat.Api/Controllers/UserController.cs
@@ -9,9 +9,24 @@ namespace SignalChat.Api.Controllers;
 [Route("api/[controller]")]
 public class UserController(IUserService userService) : BaseController
 {
+    /// <summary>
+    /// Получает информацию о пользователе по токену.
+    /// </summary>
+    /// <returns>Информация о текущем пользователе.</returns>
     [HttpGet("me")]
     public async Task<IActionResult> GetMe()
     {
         return Ok(await userService.GetUser(Id));
+    }
+
+    /// <summary>
+    /// Получает пользователя по id.
+    /// </summary>
+    /// <param name="id">Id пользователя.</param>
+    /// <returns>Информация о пользователе.</returns>
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetUser(int id)
+    {
+        return Ok(await userService.GetUser(id));
     }
 }

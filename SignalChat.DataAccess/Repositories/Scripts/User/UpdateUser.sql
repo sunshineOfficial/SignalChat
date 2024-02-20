d3f09dc903d39e80bc8615b1211038d3eda077f4
Dmitry Khrunin <thesunshineofficial@gmail.com>
2024-02-20 18:45:46 +0300

    Add coalesce to sql script

--- SignalChat.DataAccess/Repositories/Scripts/User/UpdateUser.sql
+++ SignalChat.DataAccess/Repositories/Scripts/User/UpdateUser.sql
@@ -1,7 +1,7 @@
 update users
 set
-    username = case when @Username is null then name else @Username end,
-    email = case when @Email is null then email else @Email end,
-    name = case when @Name is null then name else @Name end,
-    surname = case when @Surname is null then surname else @Surname end
+    username = coalesce(@Username, username),
+    email = coalesce(@Email, email),
+    name = coalesce(@Name, name),
+    surname = coalesce(@Surname, surname)
 where id = @Id;
